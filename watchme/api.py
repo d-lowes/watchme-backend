ac94b18fca269fba48be02dbb1dcd7d405641499
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-06-23 10:56:42 -0700

    add django app

--- watchme/api.py
+++ watchme/api.py
@@ -4,14 +4,32 @@ import requests
 
 API_SHOW_URL = "https://api.example.com/shows"
 
-def fetch_show_details(show_id):
-    """send a show ID to the API and return json"""
+
+def fetch_show_details(show_title):
+    """Send a show ID to the API and return json information about the show.
+
+    querystring = {
+        "title": show_id,
+        "country":"us",
+        "show_type":"movie",
+        "output_language":"en"
+        }
+
+    headers = {
+            "X-RapidAPI-Key": "API KEY",
+            "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com"
+    }
+
+    response = requests.get(API_SHOW_URL, headers=headers, params=querystring)
+    """
 
     try:
-        response = requests.get(f'{API_SHOW_URL}/{show_id}')
+        response = requests.get(f'{API_SHOW_URL}/{show_title}')
         response.raise_for_status()
 
         show_details = response.json()
+        return show_details
+
     except requests.exceptions.RequestException as error:
         print(f'An error occured: {error}')
         return None
